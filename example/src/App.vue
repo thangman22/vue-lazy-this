df7ef572bf65d93302f2b3402dacfb459f6d0da6
Warat Wongmaneekit <canopybanj@gmail.com>
2017-09-12 14:23:24 +0700

    Change image to dummy image

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <lazy-this>
-      <img slot="onEnter" src="https://cdn.pixabay.com/photo/2017/01/06/19/15/soap-bubble-1958650_960_720.jpg"/>
+      <img slot="onEnter" src="https://dummyimage.com/640x4:3/"/>
     </lazy-this>
   </div>
 </template>
